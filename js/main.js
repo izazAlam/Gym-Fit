5e387f7889970710642f96e8b07e0b8fbf23729b
izazAlam <izaz@echologyx.com>
2026-02-20 14:59:12 +0600

    added slider to of the wear it with section

--- js/main.js
+++ js/main.js
@@ -20,12 +20,31 @@ rightSection.addEventListener("click", () => {
   darkerBar.style.left = "156px";
 });
 
-new Swiper(".swiper", {
-  loop: true,
+const swiper = new Swiper(".swiper", {
+  loop: false,
   slidesPerView: 4,
   spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+});
+
+const prevButton = document.querySelector(".swiper-button-prev");
+const nextButton = document.querySelector(".swiper-button-next");
+const darker03 = document.querySelector(".darker-bar-03");
+
+darker03.style.left = "0px";
+
+prevButton.addEventListener("click", () => {
+  swiper.slidePrev();
+  const currentLeft = parseInt(darker03.style.left);
+
+  if (currentLeft !== 0) {
+    darker03.style.left = currentLeft - 132 + "px";
+  }
+});
+
+nextButton.addEventListener("click", () => {
+  swiper.slideNext();
+  const currentLeft = parseInt(darker03.style.left);
+  if (currentLeft <= 1180) {
+    darker03.style.left = currentLeft + 132 + "px";
+  }
 });
